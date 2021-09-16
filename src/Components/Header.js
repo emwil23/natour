baa707d18f518c17cdd7c6c66c5f07b2c3bd526b
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-09-17 00:45:01 +0530

    .

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from '../img/logo-white.png';
-import './styles.css';
+import './style.scss';
 
 const Header = () => {
   return (
     <div class='header'>
-      <div class='logo-box'>
-        <img src={logo} alt='logo' class='logo' />
+      <div class='header__logo-box'>
+        <img src={logo} alt='Logo' class='header__logo' />
       </div>
 
-      <div class='text-box'>
+      <div class='header__text-box'>
         <h1 class='heading-primary'>
-          <span class='heading-primary-main'>Outdoors</span>
-          <span class='heading-primary-sub'>is where life happens</span>
+          <span class='heading-primary--main'>Outdoors</span>
+          <span class='heading-primary--sub'>is where life happens</span>
         </h1>
 
-        <a href='#' class='btn btn-white btn-animated'>
+        <a href='' class='btn btn--white btn--animated'>
           Discover Our Tours
         </a>
       </div>
