c7ee7c8ae7764cfdf648ed2832388cacb59f7dd8
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-09-20 18:01:49 +0530

    .

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import './sass/style.scss';
+import photo1 from '../img/nat-1-large.jpg';
+import photo2 from '../img/nat-2-large.jpg';
+import photo3 from '../img/nat-3-large.jpg';
 
 const Main = () => {
   return (
     <main>
       <section className='section-about'>
-        <div className='u-center-text u-margin-bottom-8'>
+        <div className='u-center-text u-margin-bottom-big'>
           <h2 className='heading-secondary'>
             Exciting tours for adventrous people
           </h2>
         </div>
 
         <div className='row'>
-          <div className='col-1-of-2'>Text Here</div>
-          <div className='col-1-of-2'>Image Composition</div>
+          <div className='col-1-of-2'>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              You're going to fall in love with nature
+            </h3>
+            <p className='paragraph'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              Live adventures like you haven't had before
+            </h3>
+            <p className='paragraph'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s.
+            </p>
+
+            <a href='#' className='btn-text'>
+              Learn More &rarr;
+            </a>
+          </div>
+          <div className='col-1-of-2'>
+            <div className='composition'>
+              <img
+                src={photo1}
+                alt='nature-1'
+                className='composition__photo composition__photo--p1'
+              />
+              <img
+                src={photo2}
+                alt='nature-2'
+                className='composition__photo composition__photo--p2'
+              />
+              <img
+                src={photo3}
+                alt='nature-3'
+                className='composition__photo composition__photo--p3'
+              />
+            </div>
+          </div>
         </div>
       </section>
     </main>
